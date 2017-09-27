ae3fc9bf67e90e92a41f93d0bff63a356ca62b2d
Shiva Pandey <t7pash00@students.oamk.fi>
2017-09-27 13:00:43 +0300

    here it is lesson js

--- lesson.js
+++ lesson.js
@@ -1,8 +1,7 @@
 let a = (1, 10, 5, 8, 9)
 let sum = 0
-for (let index = 0; index <a; index++) {
-    let element = a [index];
-
-    
+for (let index = 0; index < 5; index++) {
+    sum = a [index];
 
 }
+ let avg = sum/ a.length
